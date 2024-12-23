75e65b4aea1bc7bf6a68a8600943736d918b7d6f
Karan Kumar <121441284+karansingh1710@users.noreply.github.com>
2024-12-23 19:17:38 +0000

    TwoDArray

---
+++ Module20/TwoDArray.java
@@ -0,0 +1,15 @@
+public class TwoDArray{
+    public static void main(String[] args) {
+        // Declartion of 2D array
+        // 2D - Array= 1D array  + 1D array
+        //               (ref)       (data)
+
+        // int[][] a;
+        // int a[][];
+        // int [][]a;
+        // int[] []a;
+        // int[] a[];
+        // int []a[];
+        
+    }
+}
