8e6dff8d348b0b9b17b9ce64a4283a532601400b
Karan Kumar <121441284+karansingh1710@users.noreply.github.com>
2025-01-17 14:47:28 +0000

    Prefix Sum 2D Array

---
+++ Module53/PrefixSum2DArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class PrefixSum2DArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int[][] mat={
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1}
+        };
+
+        System.out.println("Enter r1 and c1 in range 0-3 )");
+        int r1=sc.nextInt();
+        int c1=sc.nextInt();
+        System.out.println("Enter r2 and c2 in range 0-3 )");
+        int r2=sc.nextInt();
+        int c2=sc.nextInt();
+        int sum=0;
+        for(int i=r1; i<=r2; i++){
+            for(int j=c1; j<=c2; j++){
+                sum+=mat[i][j];
+            }
+        }
+        System.out.println("The Sum is "+sum);
+    }
+}
