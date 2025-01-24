420f107081adcc5cd9fcdb520df17e60366919ee
Karan Kumar <121441284+karansingh1710@users.noreply.github.com>
2025-01-24 16:12:58 +0000

    Binary Search Implementation

---
+++ Module56/BinarySearch.java
@@ -0,0 +1,31 @@
+package Module56;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    // Method to implement binary Search
+    // Time Complexity O(logn)
+    // Space Complexity O(1)
+    static int binarySearch(int[] arr, int target){
+        int low=0,high=arr.length-1;
+        while (low<=high) {
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of the array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter array elements:");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter target element:");
+        int target=sc.nextInt();
+    }
+}
